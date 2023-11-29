5b4324af303cebfbbd3691e0a78c3478ed607094
DanRussuleac <123557019+DanRussuleac@users.noreply.github.com>
2023-11-29 11:52:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,61 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
 from flask_cors import CORS
 import json
-mysql = MySQL()
+
 app = Flask(__name__)
 CORS(app)
-# My SQL Instance configurations
-# Change these details to match your instance configurations
+
+# MySQL configurations
 app.config['MYSQL_USER'] = 'A'
 app.config['MYSQL_PASSWORD'] = 'B'
 app.config['MYSQL_DB'] = 'student'
 app.config['MYSQL_HOST'] = 'localhost'
-mysql.init_app(app)
+mysql = MySQL(app)
 
-@app.route("/add") #Add Student
+@app.route("/add", methods=['POST'])
 def add():
-  name = request.args.get('name')
-  email = request.args.get('email')
-  cur = mysql.connection.cursor() #create a connection to the SQL instance
-  s='''INSERT INTO students(studentName, email) VALUES('{}','{}');'''.format(name,email) # kludge - use stored proc or params
-  cur.execute(s)
-  mysql.connection.commit()
+    name = request.form.get('name')
+    email = request.form.get('email')
+    cur = mysql.connection.cursor()
+    cur.execute('INSERT INTO students(studentName, email) VALUES(%s, %s);', (name, email))
+    mysql.connection.commit()
+    return jsonify({'result': 'success'})
 
-  return '{"Result":"Success"}' # Really? maybe we should check!
-  
-@app.route("/") #Default - Show Data
-def read(): # Name of the method
-  cur = mysql.connection.cursor() #create a connection to the SQL instance
-  cur.execute('''SELECT * FROM students''') # execute an SQL statment
-  rv = cur.fetchall() #Retreive all rows returend by the SQL statment
-  Results=[]
-  for row in rv: #Format the Output Results and add to return string
-    Result={}
-    Result['Name']=row[0].replace('\n',' ')
-    Result['Email']=row[1]
-    Result['ID']=row[2]
-    Results.append(Result)
-  response={'Results':Results, 'count':len(Results)}
-  ret=app.response_class(
-    response=json.dumps(response),
-    status=200,
-    mimetype='application/json'
-  )
-  return ret #Return the data in a string format
+@app.route("/update/<int:id>", methods=['POST'])
+def update(id):
+    name = request.form.get('name')
+    email = request.form.get('email')
+    cur = mysql.connection.cursor()
+    cur.execute("UPDATE students SET studentName=%s, email=%s WHERE studentID=%s", (name, email, id))
+    mysql.connection.commit()
+    return jsonify({'result': 'updated'})
+
+@app.route("/delete/<int:id>", methods=['GET'])
+def delete(id):
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM students WHERE studentID=%s", [id])
+    mysql.connection.commit()
+    return jsonify({'result': 'deleted'})
+
+@app.route("/read/<int:id>", methods=['GET'])
+def read_one(id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM students WHERE studentID=%s", [id])
+    row = cur.fetchone()
+    if row:
+        result = {'Name': row[0], 'Email': row[1], 'ID': row[2]}
+        return json.dumps(result)
+    else:
+        return jsonify({'message': 'Student not found'})
+
+@app.route("/", methods=['GET'])
+def read_all():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM students")
+    rv = cur.fetchall()
+    results = [{'Name': row[0], 'Email': row[1], 'ID': row[2]} for row in rv]
+    return jsonify({'Results': results, 'count': len(results)})
+
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',port='8080') #Run the flask app at port 8080
+    app.run(host='0.0.0.0', port=8080)
